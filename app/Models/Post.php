d68132a04f5fcd317574537aeca4b4063433f6e6
Luis Lopez <joselopezm9809@gmail.com>
2025-02-18 19:55:08 -0600

    Se termino la parte de eloquent demas se agregaron mutadores y accesores

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Mockery\CountValidator\AtMost;
 
 class Post extends Model
 {
+    //posts
+    //Category -> categories
+
     use HasFactory;
 
-    protected $table = 'posts';
-}
+    //protected $table = 'posts';
+
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            set: function($value){
+                return strtolower($value);
+
+            }, get: function($value)
+            {
+                    return ucfirst($value);
+            }
+        );
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -40,28 +40,39 @@ Route::get('/posts/{post}', function ($post) {
 */
 
 Route::get('prueba' , function(){
-
-    /*
-
-    Crear nuevo post
-
+  
+    //crear entradas nuevas 
+    /* 
     $post = new Post;
-    $post->title = 'Ttitulo de la prueba 2';
-    $post->content = 'Contenido de prueba 2';
-    $post->categoria = 'Categoria de la prueba 2';
+    $post->title = 'TiTuLo DE prueBA 4';
+    $post->content = 'Contenido de prueba 4';
+    $post->categoria = 'Categoria de la prueba 4';
 
     $post->save();
 
     return $post;
-
-    */
-
-   /*  $post = Post::find(1); buscar post por id*/
-
-    $post = Post::where('title' , 'Titulo de prueba 1') ->first();
-                /*->first();*/
+*/
+    $post = Post::find(4); //buscar post por id
+    return $post;
+   /*
+   actualizacion de un registro
+    $post = Post::where('title' , 'Ttitulo de la prueba 1')->first();
+                ////////*->first();
     $post->categoria = 'Desarrollo web';
     $post->save();
 
     return $post;
+    */
+
+    //formas de listar registros 
+    //$posts = Post::all(); //trae a pnatalla todas los registros
+   //$posts = Post::where('id' , '>=' , '2') ->get(); trae los registros mayores o iguales a dos 
+   //$posts = Post::orderBy('id' , 'desc') -> get(); muestra los registros de manera descendente
+   //$posts = Post::orderBy('categoria' , 'asc')->select('id' , 'title' , 'categoria') ->take(2)->get(); 
+
+   /* eliminar una entrada en especifico
+    $post = Post::find(1);
+    $post->delete();    
+    return "Eliminado Correctamente"; */
+
 });
